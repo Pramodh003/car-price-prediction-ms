e9c2fb8fc425afbab7701a4f1b78f0890edbf32a
pramodh003 <pramodhbr29@gmail.com>
2024-07-10 14:33:48 +0530

    fixed redis issue

--- auth/config/redis_db.py
+++ auth/config/redis_db.py
@@ -2,7 +2,7 @@ import redis
  
 from .config import settings
 
-redis_client = redis.Redis.from_url(settings.redis_url)
+redis_client = redis.Redis.from_url(settings.redis_url,decode_responses=True)
 # redis = get_redis_connection(
 #     host = settings.redis_host,
 #     port=settings.redis_port,
--- auth/utils.py
+++ auth/utils.py
@@ -1,7 +1,7 @@
 from passlib.context import CryptContext
 from .service.redis_service import check_user_in_redis, write_to_redis, read_from_redis
 from .oauth2 import get_current_user
-from fastapi import Request
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 
 
@@ -9,22 +9,13 @@ def hash(password: str):
     return pwd_context.hash(password)
 
 
-
-
-
-def verify(token, plain_password,db):
-    # if not check_user_in_redis(id):
-        cookie_authorization: str = Request.cookies.get("Authorization")
-        print(cookie_authorization)
-        # return None
-    # data = read_from_redis(username)
-    # print(data)
-    # if pwd_context.verify(plain_password, data["hashed_password"]):
-        # if data["status"] == "PASSIVE":
-        #     data["status"] = "ACTIVE"
-        #     write_to_redis(username, data)
-        #     data = read_from_redis(username)
-        # return data["id"]
-    # return False
+def verify(token, plain_password, db):
+    if not check_user_in_redis(token):
+        return None
+    data = read_from_redis(token)
+    print(data['id'])
+    if pwd_context.verify(plain_password, data["hashed_password"]):
+        return data["id"]
+    return False
 
     
