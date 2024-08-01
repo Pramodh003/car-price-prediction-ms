034638b4d45a1b58bbf10919f1a115fe3fa72faf
Pramodh BR <72683141+Pramodh003@users.noreply.github.com>
2024-08-01 18:08:59 +0530

    Update redis.py

--- auth/redis.py
+++ auth/redis.py
@@ -1,6 +1,6 @@
-from redis import Redis
-import os
-from dotenv import load_dotenv
-load_dotenv(".env")
-REDIS_URL = os.getenv('REDIS_URL')
-redis_client = Redis.from_url(REDIS_URL)
+# from redis import Redis
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(".env")
+# REDIS_URL = os.getenv('REDIS_URL')
+# redis_client = Redis.from_url(REDIS_URL)
