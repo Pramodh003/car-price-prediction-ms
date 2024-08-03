fe1ab95a8b3da5318595cc07cd72461006e9a07e
pramodh003 <pramodhbr29@gmail.com>
2024-08-03 20:42:13 +0530

    updated locals

--- eks/0-locals.tf
+++ eks/0-locals.tf
@@ -1,9 +1,9 @@
 locals {
-  env         = "staging"
+  env         = "devsecops-ms"
   region      = "us-east-1"
   zone1       = "us-east-1a"
   zone2       = "us-east-1b"
-  eks_name    = "microservice"
+  eks_name    = "devsecops-ms"
   eks_version = "1.29"
 
 }
