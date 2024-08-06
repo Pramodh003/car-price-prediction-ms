e444386579e485fee20a502b8cf8fa2c08e5bf5b
pramodh003 <pramodhbr29@gmail.com>
2024-08-06 20:57:43 +0530

    fixed service manifest

--- eks/0-locals.tf
+++ eks/0-locals.tf
@@ -1,9 +1,9 @@
 locals {
-  env         = "devsecops-ms"
+  env         = "devsecops-ms-bike"
   region      = "us-east-1"
   zone1       = "us-east-1a"
   zone2       = "us-east-1b"
-  eks_name    = "devsecops-ms"
+  eks_name    = "devops-ms-bike"
   eks_version = "1.29"
 
 }
